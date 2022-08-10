319a7905464d5309b68a40321e59acb395e5ced0
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-09 23:13:10 -0700

    commiting

--- func.c
+++ func.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * get_func - search and return the correct function
 * @s: array to check
@@ -9,9 +9,9 @@ int (*get_func(char s))(va_list)
 	op_t ops[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"d", print_d},
-		{"i", print_i},
-		{"b", print_b},
+	//	{"d", print_d},
+	//	{"i", print_i},
+	//	{"b", print_b},
 		{NULL, NULL}
 	};
 
